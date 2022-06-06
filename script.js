be85a01df34dfd5c66854a436b03f7f92f8d49c6
Miguel Soriano <originalsorianom@gmail.com>
2022-06-05 23:04:38 -0700

    Added clock to the header

---
+++ script.js
@@ -0,0 +1,4 @@
+// Using 
+var timeandday = moment().format('MMMM Do YYYY, h:mm:ss a');
+// this site help me understand  how to use the $ identifiers https://stackoverflow.com/questions/28265290/how-can-i-stop-the-current-time-and-change-it-with-js-jquery
+$("#time").html(timeandday);
